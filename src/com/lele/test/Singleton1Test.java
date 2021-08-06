cbf398c0d44354d848100e58a46266addf2ed999
FuLele <1030197720@qq.com>
2021-08-06 21:33:44 +0800

    单例模式

--- src/com/lele/test/Singleton1Test.java
+++ src/com/lele/test/Singleton1Test.java
@@ -2,6 +2,7 @@ package com.lele.test;
 
 import com.lele.single.Singleton1;
 import com.lele.single.Singleton4;
+import com.lele.single.Singleton5;
 
 public class Singleton1Test {
     public static void main(String[] args) {
@@ -12,5 +13,6 @@ public class Singleton1Test {
         //枚举类型重写了toString方法，所以输出的是名字。
         Singleton4 s1 = Singleton4.INSTANCE;
         System.out.println(s1);
+        Singleton5 s5 = Singleton5.getInstance();
     }
 }
